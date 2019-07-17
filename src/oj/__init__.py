c6e23785fcac3ddaf9006e08ef25f02c82bbbff2
Oliver Newman <orn688@gmail.com>
2019-07-16 21:53:35 -0700

    Gut

--- src/oj/__init__.py
+++
@@ -1,7 +0,0 @@
-from oj import lex, parse
-
-
-def loads(string):
-    tokens, _ = lex.lex(string)
-    # TODO: assert no remainder
-    return parse(tokens)
--- tests/test_parse.py
+++
@@ -1,8 +0,0 @@
-# from oj import tokens as t
-# from oj.parse import parse
-
-
-def test_parses_array():
-    # tokens = [t.JSONSeparator("["), t.JSONSeparator("]")]
-    # assert parse(tokens) == ([], [])
-    pass
