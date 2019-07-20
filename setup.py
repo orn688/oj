72ec3f97dcdf6aebffc92416c82c84c8e2fa6d65
Oliver Newman <orn688@gmail.com>
2019-07-20 15:54:03 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oj",
+    version="0.1",
+    description="A pure Python JSON parser",
+    author="Oliver Newman",
+    author_email="orn688@gmail.com",
+    packages=["oj"],
+    package_dir={"": "src"},
+)
